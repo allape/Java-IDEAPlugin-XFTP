4aec5d8ac81b02ffbfc216883eb619b2c385b8e2
as <allensnape@qq.com>
2020-11-04 18:03:51 +0800

    完善: 成功获取ssh配置

--- src/main/java/net/allape/actions/MenuOpenAction.java
+++ src/main/java/net/allape/actions/MenuOpenAction.java
@@ -1,44 +1,33 @@
 package net.allape.actions;
 
-import com.google.gson.Gson;
-import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationDisplayType;
-import com.intellij.notification.NotificationGroup;
-import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
-import com.intellij.openapi.ui.MessageType;
+import com.intellij.ssh.config.unified.SshConfig;
+import com.intellij.ssh.config.unified.SshConfigManager;
 import org.jetbrains.annotations.NotNull;
 
-import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.List;
 
 public class MenuOpenAction extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        NotificationGroup notificationGroup = new NotificationGroup("testid", NotificationDisplayType.BALLOON, false);
-
         try {
             // 工具包
+//            RemoteSdkUtil.getSshConfigList();
             Class util = Class.forName("com.jetbrains.plugins.remotesdk.RemoteSdkUtil");
-            // 配置内容
-            Class config = Class.forName("com.intellij.ssh.config.unified.SshConfig");
-            Field[] configFields = config.getDeclaredFields();
+            Method getSshConfigList = util.getDeclaredMethod("getSshConfigList", Project.class);
+            getSshConfigList.setAccessible(true);
 
             Project project = ProjectManager.getInstance().getDefaultProject();
-            List<Object> sshConfigs = (List<Object>) util.getDeclaredMethod("getSshConfigList", Project.class).invoke(project);
-            for (Object o : sshConfigs) {
-                for (Field f : configFields) {
-                    System.out.println(f.get(o));
-                }
+//            List<SshConfig> sshConfigs = SshConfigManager.getInstance(project).getConfigs();
+            List<SshConfig> sshConfigs = (List<SshConfig>) getSshConfigList.invoke(null, project);
+            for (SshConfig config : sshConfigs) {
+                System.out.println(config.getUsername() + "@" + config.getHost());
             }
-
-            Gson gson = new Gson();
-            Notification notification = notificationGroup.createNotification(gson.toJson(sshConfigs), MessageType.INFO);
-            Notifications.Bus.notify(notification);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
