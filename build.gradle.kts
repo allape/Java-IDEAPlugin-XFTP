b5787ee6949e646fbd3b4e4e000f6d4a85088606
as <allensnape@qq.com>
2023-08-15 22:11:25 +0800

    chore: update to support 232.*, goland is ok to use, but timeout on IDEA

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,19 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.8.20"
-    id("org.jetbrains.intellij") version "1.13.3"
+    id("org.jetbrains.kotlin.jvm") version "1.9.0"
+    id("org.jetbrains.intellij") version "1.15.0"
 }
 
 group = "net.allape"
-version = "0.10.10"
+version = "0.10.11"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.0")
-    implementation("com.google.code.gson:gson:2.9.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
+    implementation("com.google.code.gson:gson:2.10.1")
 
     compileOnly(files("/Applications/IntelliJ IDEA.app/Contents/plugins/remoteRun/lib/remoteRun.jar"))
     compileOnly(files("/Applications/IntelliJ IDEA.app/Contents/plugins/terminal/lib/terminal.jar"))
@@ -21,13 +21,13 @@ dependencies {
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version.set("IU-2023.1.1")
+    version.set("IU-2023.2")
 //    updateSinceUntilBuild.set(false)
 }
 tasks {
     patchPluginXml {
-        sinceBuild.set("230.*")
-//        untilBuild.set("232.*")
+        sinceBuild.set("232")
+//        untilBuild.set("233")
     }
 
     // Set the JVM compatibility versions
@@ -39,14 +39,3 @@ tasks {
         kotlinOptions.jvmTarget = "17"
     }
 }
-
-// TODO for future downgrade compatibility
-//sourceSets {
-//    main {
-//        @Suppress("DEPRECATION")
-//        withConvention(org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet::class) {
-//            kotlin.srcDir("src/main/kotlin")
-//            kotlin.exclude("net/allape/common/XFTPManager.kt")
-//        }
-//    }
-//}
