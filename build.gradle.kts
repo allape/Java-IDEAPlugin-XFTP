8c8dbc3c72884c04fc0ecd26932959c9fc00b4e6
as <allensnape@qq.com>
2024-08-08 16:33:39 +0800

    v0.10.14

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,32 +5,29 @@ plugins {
 }
 
 group = "net.allape"
-version = "0.10.13"
+version = "0.10.14"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.0")
+    implementation("com.google.code.gson:gson:2.10")
 
     compileOnly(files("/Applications/IntelliJ IDEA.app/Contents/plugins/remoteRun/lib/remoteRun.jar"))
     compileOnly(files("/Applications/IntelliJ IDEA.app/Contents/plugins/terminal/lib/terminal.jar"))
 }
 
-// See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
     version.set("IU-2024.1")
-    updateSinceUntilBuild.set(true)
 }
 tasks {
     patchPluginXml {
-//        sinceBuild.set("232")
-//        untilBuild.set("233")
+        sinceBuild.set("241")
+        untilBuild.set("242.*")
     }
 
-    // Set the JVM compatibility versions
     withType<JavaCompile> {
         sourceCompatibility = "17"
         targetCompatibility = "17"
